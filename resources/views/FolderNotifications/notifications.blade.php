ef5b672096de52241ae52f8e18547999a2917cc1
Kemal <kemal.naufal0@gmail.com>
2025-05-15 15:00:27 +0700

    Notifikasi dari waktu laporan desc

--- resources/views/FolderNotifications/notifications.blade.php
+++ resources/views/FolderNotifications/notifications.blade.php
@@ -7,27 +7,32 @@
         <h2 class="text-2xl font-bold text-green-900">Notifikasi</h2>
     </div>
 
-    {{-- Isi Notifikasi --}}
-    <div class="p-6">
-        @if(session('report'))
-            @foreach(session('report') as $notif)
-                <div class="bg-[#DDFFAC] rounded-xl p-3 mb-4 cursor-pointer hover:bg-green-300 transition"
-                    onclick="window.location.href='{{ route('gotoMapping', ['lat' => $notif['lat'], 'lng' => $notif['lng']]) }}'">
+ <div class="p-6">
+    @if(session('report'))
+        @php
+            $notifs = collect(session('report'))->sortByDesc('waktu_laporan');
+        @endphp
 
-                    {{-- Baris nama staf dan waktu --}}
-                    <div class="flex justify-between items-center">
-                        <div class="font-semibold">{{ $notif['namaStaf'] }}</div>
-                        <div class="text-sm text-gray-600">
-                            {{ \Carbon\Carbon::parse($notif['waktu_laporan'])->diffForHumans() }}
-                        </div>
+        @foreach($notifs as $notif)
+            <div class="bg-[#DDFFAC] rounded-xl p-3 mb-4 cursor-pointer hover:bg-green-300 transition"
+                onclick="window.location.href='{{ route('gotoMapping', ['lat' => $notif['lat'], 'lng' => $notif['lng']]) }}'">
+
+                {{-- Baris nama staf dan waktu --}}
+                <div class="flex justify-between items-center">
+                    <div class="font-semibold">{{ $notif['namaStaf'] }}</div>
+                    <div class="text-sm text-gray-600">
+                        {{ \Carbon\Carbon::parse($notif['waktu_laporan'])->diffForHumans() }}
                     </div>
-
-                    {{-- Isi pesan --}}
-                    <div>baru saja menambahkan data lapangan</div>
                 </div>
-            @endforeach
-        @else
-            <p>Tidak ada notifikasi.</p>
-        @endif
+
+                {{-- Isi pesan --}}
+                <div>baru saja menambahkan data lapangan</div>
+            </div>
+        @endforeach
+    @else
+        <p>Tidak ada notifikasi.</p>
+    @endif
+</div>
+
     </div>
 @endsection
