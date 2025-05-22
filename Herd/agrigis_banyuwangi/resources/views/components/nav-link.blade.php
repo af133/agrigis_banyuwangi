6e139b8c74b2528cdea245afd6ebf1ccacbae458
Andre Firmansyah <166293262+af133@users.noreply.github.com>
2025-05-22 19:17:43 +0700

    Delete Herd/agrigis_banyuwangi directory

--- Herd/agrigis_banyuwangi/resources/views/components/nav-link.blade.php
+++
@@ -1,3 +0,0 @@
-@props(['active'=>false])
-<a {{ $attributes }} class="{{ $active ? 'bg-[#0D6106] text-white':'text-black hover:bg-[#579251]  hover:text-white' }} text-black font-bold  block px-3 py-2 rounded-md hover:bg-[#579251]  ">{{ $slot }}
-</a>
